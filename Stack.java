e2692e588bae4a5381c7e71d175b0675f9529709
scintillatingmks <mantosh675@gmail.com>
2019-09-02 18:51:15 +0530

    modified stack

--- Stack.java
+++ Stack.java
@@ -3,30 +3,44 @@ package com.company;
 import java.util.Scanner;
 
 public class Stack {
+    static int top=-1,stack[];
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the size of an stack :");
         int n = sc.nextInt();
-        int stack1[] = new int[n];
+        stack = new int[n];
         System.out.println("enter the item for inserting :");
-        int top=-1;
-        push(stack1,top);
-        display(stack1);
+        push(stack);
+        display(stack);
+        pop();
+        display(stack);
     }
-    public static void push(int [] stack,  int top  ){
-        int n=stack.length;
+    public static void push(int [] stack){
+
+        int n = stack.length;
         Scanner s=new Scanner(System.in);
         int item=s.nextInt();
-        if ( top == n) {
+
+        if ( top == n-1) {
             System.out.println("overflow");
         }else {
             stack[++top] = item;
-            push(stack,top);
+            push(stack);
         }
     }
     public static void display(int [] stack){
-        for (int i = 0; i < stack.length; i++) {
-            System.out.print(stack[i]);
+        for (int i = 0; i < top; i++) {
+            System.out.println(stack[i]);
+        }
+    }
+    public static void pop(){
+        if(top==-1){
+            System.out.println("underflow");
+        }
+        else {
+            System.out.println(stack[top]+ "is poped\n");
+            top=top-1;
+            pop();
         }
     }
 }
