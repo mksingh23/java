79a634d07c1ae1b70f4a4340f33c717b9047d99f
scintillatingmks <mantosh675@gmail.com>
2019-10-15 23:24:19 +0530

    adding Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,49 @@
 package com.company;
 
+import java.util.Scanner;
+
 abstract public class Calculator {
     double a;
     double b;
     double result;
     public abstract void evaluate();
     public abstract void display();
+    public static void main(String[] args) {
+        System.out.println("implementation of interface");
+        System.out.println("Enter two digit for calculation :");
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        System.out.println("Welcome to Calculator :-");
+        System.out.println("1. Addition ");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. divide");
+        System.out.println("Enter your choice ...");
+        int choice = sc.nextInt();
+        switch (choice){
+            case 1:
+                Addition ad = new Addition(first,second);
+                ad.evaluate();
+                ad.display();
+                break;
+            case 2:
+                Subtraction sb = new Subtraction(first,second);
+                sb.evaluate();
+                sb.display();
+                break;
+            case 3:
+                Multiplication ml = new Multiplication(first,second);
+                ml.evaluate();
+                ml.display();
+                break;
+            case 4 :
+                Devide dv = new Devide(first,second);
+                dv.evaluate();
+                dv.display();
+                break;
+        }
+    }
 }
 class Addition extends Calculator{
     public Addition(double x,double y){
@@ -69,5 +107,7 @@ class Devide extends Calculator{
     public void display() {
         System.out.println("the result of this expression is :"+a+"/"+b+"="+result);
     }
+
+
 }
 
